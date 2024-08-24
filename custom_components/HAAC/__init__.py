cfce5c6a80ac425037471106a3094477529a6702
Nick <nick_louwes1@hotmail.com>
2024-08-24 16:59:49 +0200

    fix deprecations shown in 2024.8

--- custom_components/HAAC/__init__.py
+++ custom_components/HAAC/__init__.py
@@ -1,5 +1,5 @@
-
 """GitHub Custom Component."""
+
 import logging
 
 from homeassistant import config_entries, core
@@ -17,9 +17,7 @@ async def async_setup_entry(
     hass.data[DOMAIN][entry.entry_id] = entry.data
 
     # Forward the setup to the sensor platform.
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 
